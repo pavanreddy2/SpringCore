4a19a190e7f48fcb96d7bad7fa4bdca1ca41b93c
AR Institute <bpavankumar402@gmail.com>
2024-11-07 09:41:35 +0530

    added config files

--- SpringCore/SpringCore-1-XMLConfig/src/main/java/org/example/Main.java
+++ SpringCore/SpringCore-1-XMLConfig/src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import org.example.pojo.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String cfg = "cfg.xml";
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext(cfg);
+        Student student = (Student) applicationContext.getBean("stdId");
+        student.display();
+        System.out.println();
+        Student student1 = (Student) applicationContext.getBean("stdId2");
+        student1.display();
     }
 }
